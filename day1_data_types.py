797a0ca25134780cdc7d64d0798fa6e281891dc0
trijal goel <trijalgoel216@gmail.com>
2026-04-20 23:59:39 +0530

    calculator

--- day1_data_types.py
+++ day1_data_types.py
@@ -1,11 +1,4 @@
 #Data types
-
-character_name = "Tom"
-character_age = 50
-is_Male = False
-print("There once was a man named " + character_name + ", ")
-print("he was "+ character_age+ " years old. ")
-
-character_name = "Mike"
-print("He really liked the name " + character_name + ", ")
-print("but didn't like being " + character_age + ".")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "! You are " + age)
