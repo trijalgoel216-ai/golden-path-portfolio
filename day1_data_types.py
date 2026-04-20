e274665f33dee141ee3c46924653eac552bf2ceb
trijal goel <trijalgoel216@gmail.com>
2026-04-20 22:40:27 +0530

    Data types

---
+++ day1_data_types.py
@@ -0,0 +1,11 @@
+#Data types
+
+character_name = "Tom"
+character_age = 50
+is_Male = False
+print("There once was a man named " + character_name + ", ")
+print("he was "+ character_age+ " years old. ")
+
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
