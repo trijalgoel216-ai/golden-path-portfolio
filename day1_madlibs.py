561558b7e00c428b53f7a4652cbdc038b5ea3fd4
trijal goel <trijalgoel216@gmail.com>
2026-04-21 00:08:12 +0530

    madlibs

---
+++ day1_madlibs.py
@@ -0,0 +1,10 @@
+#Madlibs
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+
+print("Roses are " + color)
+print( plural_noun + " are blue")
+print("I love " + celebrity)
