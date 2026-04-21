0d5dceb62839a9a1719ccd33c9b20baacc9efa25
trijal goel <trijalgoel216@gmail.com>
2026-04-21 23:30:45 +0530

    try except

--- loops.py
+++ loops.py
@@ -5,3 +5,7 @@ def raise_to_power(base_num, pow_num):
         result = result * base_num
     return result
 print(raise_to_power(4,4))
+
+'''
+dscfvrtbrtbrtbrtbr
+'''
--- translator.py
+++ translator.py
@@ -1,1 +1,13 @@
 #Translator
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter 
+    return translation
+print(translate(input("Enter a phrase: ")))
