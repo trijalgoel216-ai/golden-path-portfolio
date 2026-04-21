3231b795fb7e938d0c0e530cbb138ea77cdff7dc
trijal goel <trijalgoel216@gmail.com>
2026-04-21 22:25:18 +0530

    functions

--- functions.py
+++ functions.py
@@ -1,14 +1,8 @@
 #Functions
-def max_num(num1, num2, num3) :
-    if num1 >= num2 and num1 >= num3:
-      return num1
-    elif num2 >= num1 and num2 >= num3:
-       return num2
-    else:
-       return num3
-    
 
+i = 1
+while i<=10: 
+    print(i)
+    i += 1
 
-
-
-print(max_num(393992,5,6))
+print("Done with loop")
