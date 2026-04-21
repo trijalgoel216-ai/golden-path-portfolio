d431c481b4cf2f867f0698b9891150f35417bc5c
trijal goel <trijalgoel216@gmail.com>
2026-04-21 17:33:13 +0530

    better calculator

--- day1_calculator.py
+++ day1_calculator.py
@@ -1,6 +1,15 @@
 #Calcualtor 
-num1 = input("Enter a number: ")
-num2= input("Enter another number: ")
-result = float(num1) + float(num2)
+num1 = float(input("Enter first number: "))
+op = input("Enter operator : ")
+num2 = float(input("Enter second number: "))
 
-print(result)
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1/num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid operation")
