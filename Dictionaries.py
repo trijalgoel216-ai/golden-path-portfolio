ebcd877c8f9c48c2118a9f0ff6c0e96a27b1c182
trijal goel <trijalgoel216@gmail.com>
2026-04-21 22:02:15 +0530

    dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,17 @@
+#Dictionaries
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions.get("Luv", "Not a valid Key"))
